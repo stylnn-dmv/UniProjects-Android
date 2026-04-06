d9df66bbc7165278946dc7980d85844e3d5f8a79
Stilyan <stili4771@gmail.com>
2026-04-06 18:48:01 +0300

    Moved C# files into folder

---
+++ C#/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Welcome.Model;
+using Welcome.View;
+using Welcome.ViewModel;
+
+
+namespace Welcome
+{
+  
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            User obj1 = new User("Ivan Todorov","Pass1234","ivantd@gmail.com",Others.UserRolesEnum.STUDENT,0);
+            UserViewModel obj2 = new UserViewModel(obj1);
+            MainWindow obj3 = new MainWindow(obj2);
+            obj3.DisplayUser();
+            obj3.Show();
+            
+
+        }
+    }
+
+}
